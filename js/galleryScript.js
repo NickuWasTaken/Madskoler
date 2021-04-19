4bc429d38324766b0462edec9d73072bfeebbf85
NickuWasTaken <74908215+NickuWasTaken@users.noreply.github.com>
2021-04-19 16:09:12 +0200

    Opdatering til Galleri JS
    Co-Authored-By: anderslicht <45091587+anderslicht@users.noreply.github.com>

--- js/galleryScript.js
+++ js/galleryScript.js
@@ -1,7 +1,33 @@
 function myFunction(imgs) {
-  var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
-  expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
-  expandImg.parentElement.style.display = "block";
+	var expandImg = document.getElementById("expandedImg");
+	var NavID = imgs.id;
+
+	expandImg.src = imgs.src;
+	expandImg.parentElement.style.display = "block";
+}
+
+
+function galleri(img){
+	var imgID = img.id;
+	document.getElementById("galleri-nav").innerHTML = "";
+
+	var galleryImg = ['frivillig1.gif', 'frivillig2.jpg', 'frivillig3.gif', 'frivillig4.jpg'];
+
+	for (var i = galleryImg.length - 1; i >= 0; i--) {
+		var image = document.createElement("img");
+		var content = document.createTextNode(galleryImg[i]);
+		
+		image.setAttribute("id", "fri" + i);
+		image.setAttribute("src", "img/" + galleryImg[i]);
+		image.setAttribute("onclick", "myFunction(this); galleri(this);");
+		image.setAttribute("class", "small");
+
+		var imgIDCheck = image.getAttribute("id");
+		
+		if (imgIDCheck == imgID) {
+			image.setAttribute("style", "display:none;");
+		}
+
+		document.getElementById("galleri-nav").appendChild(image);
+	}
 }
